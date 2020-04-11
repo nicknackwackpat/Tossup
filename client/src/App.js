ca9eb0890152c1ca2b4581ccc07ec72dbe656762
thegetoutgirl <amyahaha@gmail.com>
2020-04-11 16:31:11 -0400

    navbar update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import Nav from "./components/Nav";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // The app will not render correctly until you setup a Route component.
 // Refer to the Basic Example documentation if you need to.
 // (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
     <div>
-
+    <Navbar />
     </div>
-    </BrowserRouter>
+    // </BrowserRouter>
   );
 }
 
