7f3d80b473ec1a0f0976495989684bd01eca9c2a
thegetoutgirl <amyahaha@gmail.com>
2020-04-11 19:28:58 -0400

    fixed database and front-end routing error

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/navbar.js";
 // import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // The app will not render correctly until you setup a Route component.
@@ -10,23 +10,6 @@ function App() {
     // <BrowserRouter>
     <div>
     <Navbar />
-    import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
-
-const Example = (props) => {
-  return (
-    <div>
-      <Jumbotron fluid>
-        <Container fluid>
-          <h1 className="display-3">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        </Container>
-      </Jumbotron>
-    </div>
-  );
-};
-
-export default Example;
     </div>
 
     // </BrowserRouter>
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const passportLocalMongoose = require('passport-local-mongoose');
+// const passportLocalMongoose = require('passport-local-mongoose');
  
 const userSchema = new Schema({
 
@@ -15,39 +15,26 @@ const userSchema = new Schema({
     trim: true,
     required: "Last Name is Required"
   },
-  state: {
+  email: {
     type: String,
     trim: true,
-    required: "States is Required"
+    required: "Email is Required",
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
   },
- 
-  username: {
-    type: String,
-    trim: true,
-    required: "Username is Required"
-  },
-
   password: {
     type: String,
     trim: true,
     required: "Password is Required",
     validate: [({ length }) => length >= 6, "Password should be longer."]
   },
-
-  email: {
-    type: String,
-    unique: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-  },
-
   userCreated: {
     type: Date,
     default: Date.now
   },
 
-  candidate1:  String,
-  candidate2:  String ,
-  color: String
+  // candidate1:  String,
+  // candidate2:  String ,
+  // color: String
 
 });
 
