8507dc5a044be177be43470db12de7daaaf37ba7
Nelson Ezeume <nelsonezeume@yahoo.com>
2020-04-13 12:32:01 -0400

    updated component

---
+++ client/src/components/candidateSelect/candidate.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Candidate = (props)=> {
+
+    return(
+        <div>
+        <h1>Needs work</h1>
+        </div>
+    );
+    
+};
+
+
+
+
+
+
+export default Candidate;
