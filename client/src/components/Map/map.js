617ac4bbba378b908832291d16e452bb771574c6
nicknack <nicknicknick22@aol.com>
2020-04-13 15:17:17 -0400

    Updated map component

--- client/src/components/Map/map.js
+++ client/src/components/Map/map.js
@@ -1,40 +1,33 @@
-import React from 'react';
-import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
-import 'map.css';
+import React, { Component } from 'react';
+import './map.css';
+import USAMap from "react-usa-map";
 
-export default function Map() {
-    componentDidMount() { 
+class Map extends Component {
 
-    const svg = d3.select("svg");
-    
-    const path = d3.geoPath();
-    
-    d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
-      if (error) throw error;
-    
-      svg.append("g")
-          .attr("class", "states")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
-        .enter().append("path")
-          .attr("d", path);
-    
-      svg.append("path")
-          .attr("class", "state-borders")
-          .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-    });
+  mapHandler = (event) => {
+    alert(event.target.dataset.name);
+  };
 
-    d3.csv("statePop.csv", function(data) {
-    // color.domain([0,1,2,3]);
-    // stateName = data.feature.name;
-    });
+  /* optional customization of filling per state and calling custom callbacks per state */
+  statesCustomConfig = () => {
+    return {
+      "NJ": {
+        fill: "navy",
+        clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
+      },
+      "NY": {
+        fill: "#CC0000"
+      }
     };
-    
-return (
-    <Map>
-    <svg width="960" height="600"></svg>
-    </Map>
-)
+  };
+
+  render() {
+    return (
+      <div className="map">
+        <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
+      </div>
+    );
+  }
 }
-    
+
+export default Map;
