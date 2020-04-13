a6e8a07e0b99bca46a552a592d17970aa4f1ecde
Amy <60320896+thegetoutgirl@users.noreply.github.com>
2020-04-13 12:47:12 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-module.exports = {
-    User: require("./user"),
-    // Map: require("./map")
-  };
-=======
 //Exporting an object containing all of our models
 
 module.exports = {
     User: require("./user"),
     // Map: require("./map")
 };
->>>>>>> 2dac782333516a45039b683fe315b1d595d908a3
