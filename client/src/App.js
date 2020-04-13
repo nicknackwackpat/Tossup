0fe9334c28eccb99fa71e61810c5c63d55e4ffd5
Joe Vander Neut <jpvanderneut@gmail,com>
2020-04-13 15:22:24 -0400

    fixed errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,41 @@
-import React from "react";
-import TopMenu from "./components/Navbar/navbar.js";
-import Carousel from "./components/Carousel/Carousel.js"
-// import Header from "./components/Header/Header.js"
-import Map from "./components/Map/map.js";
+// import React from "react";
+// import TopMenu from "./components/Navbar/navbar.js";
+// import Carousel from "./components/Carousel/Carousel.js"
+// // import Header from "./components/Header/Header.js"
+// // import Footer from "./components/Footer/Footer.js"
+// import Map from "./components/Map/map.js";
 
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import CandidatePage from " ./client/srs/pages/page.js"
+// import ElectionPage from " ./client/srs/pages/Electionpage.js"
+// import LoginPage from " ./client/srs/pages/Loginpage.js"
+// import ProfilePage from " ./client/srs/pages/Profilepage.js"
+// import ResultsPage from " ./client/srs/pages/Resultspage.js"
+// import SignUpPage from " ./client/srs/pages/SignUppage.js"
+// //import Navbar from "./components/Navbar/Navbar.js"
 
-// BrowserRouter instructions
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
+// // import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    // <BrowserRouter>
-    <div>
-    <TopMenu />
-    <Carousel />
-    <Map />
-    </div>
+// // BrowserRouter instructions
+// // The app will not render correctly until you setup a Route component.
+// // Refer to the Basic Example documentation if you need to.
+// // (https://reacttraining.com/react-router/web/example/basic)
 
-    // </BrowserRouter>
-  );
-}
+// function App() {
+//     return (
+//       <Router>
+//         <div>
+//           <Nav/>
+//           <Switch>
+//       <Route exact path ={["/", "/"]}
+//     <TopMenu />
+//     <Carousel />
+//     <Map />
+// </Switch>
+//     </div>
+//       </Router>
+//   );
+//   }
+// }
 
-export default App;
+// export default App;
+
