19d28d64204f0218cf8a9f401a72e2e2a10f5648
Joe Vander Neut <jpvanderneut@gmail,com>
2020-04-14 23:25:14 -0400

    footer

--- client/src/Pages/CandidatePage.js
+++ client/src/Pages/CandidatePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TopMenu from "./../components/Navbar/Navbar.js";
-
+import Footer from "./../components/Footer/Footer.js";
 function CandidatePage (){
     return (
         <div>
--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -3,6 +3,7 @@ import TopMenu from "../components/Navbar/Navbar.js";
 import Carousel from "../components/Carousel/Carousel.js"
 import LoginForm from "../components/Login/Login.js"
 import SignupExpander from "../components/Login/Expander.js"
+import Footer from "./../components/Footer/Footer.js"
 
 function LoginPage() {
   return (
@@ -11,6 +12,7 @@ function LoginPage() {
     <Carousel />
     <br />
     <LoginForm />  <SignupExpander />
+    <Footer/>
     </div>
   );
 }
