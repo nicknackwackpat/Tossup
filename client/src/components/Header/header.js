1c352305cffae5a81e7b3b7ce031e47b88ea7ce7
nicknack <nicknicknick22@aol.com>
2020-04-17 16:17:55 -0400

    restored header.js and footer.css

---
+++ client/src/components/Header/header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Jumbotron, Container } from "reactstrap";
+import "./header.css";
+import {Row, Col } from 'reactstrap';
+// import "../../styles.css";
+// This is the header for all pages except the Home page!
+
+const Header = (props) => {
+  return (
+    <div>
+      <Jumbotron fluid className="mainbkg">
+        <Container>
+        <Row>
+          <Col xs="6" className="header-right">
+          <img className="header-img" src="https://i.imgur.com/EQBHicX.png " alt="podium-guy" />
+          </Col>
+          <Col xs="6">
+          <h1 className="header-style">TossUp!</h1>
+          <p className="lead"></p>
+
+          </Col>
+      </Row>
+
+        </Container>
+      </Jumbotron>
+      </div>
+    )};
+
+export default Header;
