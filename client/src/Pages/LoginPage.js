4c6dd6640f0177bacd3aae2c777aaaed09f53821
thegetoutgirl <amyahaha@gmail.com>
2020-04-17 16:22:00 -0400

    merging

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -2,11 +2,12 @@ import React from "react";
 import Carousel from "../components/Carousel/carousel.js"
 import LoginForm from "../components/Login/login.js"
 import SignupExpander from "../components/Login/expander.js"
+import Header from "../components/Header/header.js";
 
 function LoginPage(props) {
   return (
     <div className="container">
-    <Carousel />
+    <Header />
     <br />
     <div className="container login-form">
     <LoginForm className="login-form" handleInputChange={props.handleInputChange} login={props.login} email={props.email} password={props.password} />  <SignupExpander />
